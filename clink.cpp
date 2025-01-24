1236ee82893ae1112de60e176d85004e387d4f4c
faridhakanda <faridakanda1729@gmail.com>
2025-01-24 17:36:40 +0600

    linked list

--- clink.cpp
+++ clink.cpp
@@ -1,14 +1,13 @@
-// doubly linked list 
+// circular linked list
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef struct node {
     int data;
     struct node *next;
-    struct node *prev;
 } Node;
 
 int main() {
-    
+
     return 0;
 }
