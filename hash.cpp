2e671e9d19066293cfb42ed0a425e8bd256fe90e
faridhakanda <faridakanda1729@gmail.com>
2025-02-04 14:53:54 +0600

    hash map updated

--- hash.cpp
+++ hash.cpp
@@ -1,7 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef struct hashNode {
-};
+typedef struct hashnode {
+    string name;
+    string address;
+} hashNode;
+
+int hashFunction(string name) {
+    int n = size(name);
+    return n % 10;
+}
 int main() {
 
     return 0;
